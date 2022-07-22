ae44b7585aee72259f3870b0e6d8b9cbddc68fb4
namitrathod1 <namit.rathod@cognizant.com>
2022-07-22 16:28:39 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'everyone ';
+
+  headers = ['Name', 'Car'];
+  details = [
+    {
+      Name: 'Shyam',
+      Car: 'BMW',
+    },
+    {
+      Name: 'Rohan',
+      Car: 'mercedes',
+    },
+    {
+      Name: 'Shubham',
+      Car: 'Audi',
+    },
+  ];
 }
